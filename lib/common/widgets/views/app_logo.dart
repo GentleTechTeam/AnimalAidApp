d3fe5d481068053c4c7ec70341fcae4f8ae0fc00
GentleTechTeam <gentletechteam@gmail.com>
2024-04-17 13:09:42 +0700

    fix: Logo displaying fixes, build fixes. Added more mocks

--- lib/common/widgets/views/app_logo.dart
+++ lib/common/widgets/views/app_logo.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 class AppLogo extends StatelessWidget {
   const AppLogo({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) => Image.asset(
-        'assets/images/logo_with_text.png',
-        width: 300,
-        height: 300,
+  Widget build(BuildContext context) => Container(
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+        ),
+        child: Image.asset(
+          'assets/images/logo_with_text.png',
+          width: 300,
+          height: 300,
+        ),
       );
 }
--- lib/posts_feed/services/posts_feed_service_mock.dart
+++ lib/posts_feed/services/posts_feed_service_mock.dart
@@ -10,19 +10,30 @@ class PostsFeedServiceMock extends PostsFeedService {
     String? locationKey,
     DateTime? fromDate,
   }) async {
-    final groupBuilder1 = GPostsFeedData_postsFeed_items_groupBuilder()
+    final firstPostGroupBuilder = GPostsFeedData_postsFeed_items_groupBuilder()
       ..id = '1'
       ..name = 'Pet Haven'
       ..groupPhotoUrl =
           'https://images.freeimages.com/variants/SarTues7r2UAXiYjEcu2Pss9/f4a36f6589a0e50e702740b15352bc00e4bfaf6f58bd4db850e167794d05993d?fmt=webp&h=350'
       ..G__typename = 'PostGroup';
 
-    final groupBuilder2 = GPostsFeedData_postsFeed_items_groupBuilder()
+    final firstPostPhotoUrlBuilder = ListBuilder<String>()
+      ..addAll([
+        'https://images.freeimages.com/images/large-previews/4a6/pet-1543980.jpg?fmt=webp&h=350',
+        'https://images.freeimages.com/images/large-previews/559/cat-1362565.jpg?fmt=webp&h=350',
+      ]);
+
+    final secondPostGroupBuilder = GPostsFeedData_postsFeed_items_groupBuilder()
       ..id = '1'
       ..name = 'Paw Palace'
       ..groupPhotoUrl =
           'https://images.freeimages.com/variants/vFK5rP2CBFdqNYPR6N1KqZm4/f4a36f6589a0e50e702740b15352bc00e4bfaf6f58bd4db850e167794d05993d?fmt=webp&h=350'
       ..G__typename = 'PostGroup';
+
+    final secondPostPhotoUrlBuilder = ListBuilder<String>()
+      ..add(
+        'https://images.freeimages.com/images/large-previews/957/street-dog-1570987.jpg?fmt=webp&h=350',
+      );
 
     final groupBuilder3 = GPostsFeedData_postsFeed_items_groupBuilder()
       ..id = '1'
@@ -30,6 +41,11 @@ class PostsFeedServiceMock extends PostsFeedService {
       ..groupPhotoUrl =
           'https://images.freeimages.com/images/large-previews/537/dog-1550881.jpg?fmt=webp&h=350'
       ..G__typename = 'PostGroup';
+
+    final thirdPostPhotoUrlBuilder = ListBuilder<String>()
+      ..add(
+        'https://images.freeimages.com/images/large-previews/cb1/cat-1391168.jpg?fmt=webp&h=350',
+      );
 
     return PostsFeedResponse(
       data: [
@@ -39,8 +55,9 @@ class PostsFeedServiceMock extends PostsFeedService {
             ..description =
                 'Pets, like cats and dogs, bring boundless joy into our lives. Their playful antics and unwavering loyalty make every day brighter.'
             ..G__typename = 'Post'
-            ..group = groupBuilder1
+            ..group = firstPostGroupBuilder
             ..url = 'https://www.google.com'
+            ..photoAttachmentUrls = firstPostPhotoUrlBuilder
             ..creationDate = '2021-09-03T21:00:00Z',
         ),
         GPostsFeedData_postsFeed_items(
@@ -49,8 +66,9 @@ class PostsFeedServiceMock extends PostsFeedService {
             ..description =
                 "Welcoming a pet into your home isn't just about gaining a companion; it's about embracing a furry family member who enriches your life with love and laughter."
             ..G__typename = 'Post'
-            ..group = groupBuilder2
+            ..group = secondPostGroupBuilder
             ..url = 'https://www.google.com'
+            ..photoAttachmentUrls = secondPostPhotoUrlBuilder
             ..creationDate = '2021-09-02T10:00:00Z',
         ),
         GPostsFeedData_postsFeed_items(
@@ -61,6 +79,7 @@ class PostsFeedServiceMock extends PostsFeedService {
             ..G__typename = 'Post'
             ..group = groupBuilder3
             ..url = 'https://www.google.com'
+            ..photoAttachmentUrls = thirdPostPhotoUrlBuilder
             ..creationDate = '2021-09-01T02:00:00Z',
         ),
       ].toBuiltList(),
